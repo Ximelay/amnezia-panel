b1d0e264600761b372d67cfe97df98b20c1b187b
slowy19 <135140649+slowy19@users.noreply.github.com>
2026-03-25 19:09:40 +0300

    Update time2pay.sh

--- scripts/time2pay.sh
+++ scripts/time2pay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ENV_FILE=$(find /root/amnezia-panel /home/amnezia-panel /opt/amnezia-panel -name ".env" -type f 2>/dev/null | head -n 1)
+ENV_FILE=$(find / -type f -name ".env" -path "*/amnezia-panel/*" 2>/dev/null | head -n 1)
 
 if [ -n "$ENV_FILE" ]; then
     source "$ENV_FILE"
